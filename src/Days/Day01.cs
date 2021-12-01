d09da7ef7dd9249803f6e0ae0872d9f671e97278
Dylan Smith <dylan-smith@github.com>
2021-11-30 23:47:40 -0600

    formatting

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -10,7 +10,9 @@ namespace AdventOfCode.Days
         public override string PartOne(string input)
         {
             var depths = input.Integers().ToArray();
-            var count = depths.SelectWithIndex().Skip(1).Count(x => x.item > depths[x.index - 1]);
+            var count = depths.SelectWithIndex()
+                              .Skip(1)
+                              .Count(x => x.item > depths[x.index - 1]);
 
             return count.ToString();
         }
@@ -18,8 +20,13 @@ namespace AdventOfCode.Days
         public override string PartTwo(string input)
         {
             var depths = input.Integers().ToArray();
-            depths = depths.SelectWithIndex().Skip(2).Select(x => x.item + depths[x.index - 1] + depths[x.index - 2]).ToArray();
-            var count = depths.SelectWithIndex().Skip(1).Count(x => x.item > depths[x.index - 1]);
+            depths = depths.SelectWithIndex()
+                           .Skip(2)
+                           .Select(x => x.item + depths[x.index - 1] + depths[x.index - 2])
+                           .ToArray();
+            var count = depths.SelectWithIndex()
+                              .Skip(1)
+                              .Count(x => x.item > depths[x.index - 1]);
 
             return count.ToString();
         }
