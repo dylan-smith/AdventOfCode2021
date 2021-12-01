89a332321c14b7f45ec2121670488096c74c58e0
Dylan Smith <dylan-smith@github.com>
2021-11-30 23:06:51 -0600

    Day 1 solved

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode.Days
 {
@@ -7,12 +9,50 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            return string.Empty;
+            var depths = input.Integers();
+
+            var cur = depths.First();
+
+            var increases = 0;
+
+            for (var i = 0; i < depths.Count(); i++)
+            {
+                if (depths.ElementAt(i) > cur)
+                {
+                    increases++;
+                }
+
+                cur = depths.ElementAt(i);
+            }
+
+            return increases.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            return string.Empty;
+            var depths = input.Integers().ToArray();
+
+            var a = 0;
+            var count = 0;
+
+            var sum = depths[a] + depths[a + 1] + depths[a + 2];
+
+            a++;
+
+            while (a + 2 < depths.Length)
+            {
+                var newSum = depths[a] + depths[a + 1] + depths[a + 2];
+
+                if (newSum > sum)
+                {
+                    count++;
+                }
+
+                sum = newSum;
+                a++;
+            }
+
+            return count.ToString();
         }
     }
 }
