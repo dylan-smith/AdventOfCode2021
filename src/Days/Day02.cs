f2b6b052dd1808d4391fb55930759879ba04d975
Dylan Smith <dylan-smith@github.com>
2021-12-01 23:08:55 -0600

    day 2

--- src/Days/Day02.cs
+++ src/Days/Day02.cs
@@ -1,19 +1,63 @@
-﻿using System;
-using System.Linq;
+﻿namespace AdventOfCode.Days;
 
-namespace AdventOfCode.Days
+[Day(2021, 2)]
+public class Day02 : BaseDay
 {
-    [Day(2021, 2)]
-    public class Day02 : BaseDay
+    public override string PartOne(string input)
     {
-        public override string PartOne(string input)
+        var commands = input.Lines();
+        var hor = 0;
+        var depth = 0;
+
+        foreach (var command in commands)
         {
-            return string.Empty;
+            var instruction = command.Words().First();
+            var value = long.Parse(command.Words().Last());
+
+            switch (instruction)
+            {
+                case "forward":
+                    hor += int.Parse(command.Split(' ')[1]);
+                    break;
+                case "down":
+                    depth += int.Parse(command.Split(' ')[1]);
+                    break;
+                case "up":
+                    depth -= int.Parse(command.Split(' ')[1]);
+                    break;
+            }
         }
 
-        public override string PartTwo(string input)
+        return (hor * depth).ToString();
+    }
+
+    public override string PartTwo(string input)
+    {
+        var commands = input.Lines();
+        var hor = 0;
+        var depth = 0;
+        var aim = 0;
+
+        foreach (var command in commands)
         {
-            return string.Empty;
+            if (command.Split(' ')[0] == "forward")
+            {
+                hor += int.Parse(command.Split(' ')[1]);
+                depth += int.Parse(command.Split(' ')[1]) * aim;
+            }
+
+            if (command.Split(' ')[0] == "down")
+            {
+                aim += int.Parse(command.Split(' ')[1]);
+            }
+
+            if (command.Split(' ')[0] == "up")
+            {
+                aim -= int.Parse(command.Split(' ')[1]);
+            }
         }
+
+        return (hor * depth).ToString();
     }
 }
+
