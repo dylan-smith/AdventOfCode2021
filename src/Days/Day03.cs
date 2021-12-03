f24b510fac6234fbf86e813512cee43573c3ac85
Dylan Smith <dylan-smith@github.com>
2021-12-02 23:19:33 -0600

    day 3 solved

--- src/Days/Day03.cs
+++ src/Days/Day03.cs
@@ -1,19 +1,86 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿namespace AdventOfCode.Days;
 
-namespace AdventOfCode.Days
+[Day(2021, 3)]
+public class Day03 : BaseDay
 {
-    [Day(2021, 3)]
-    public class Day03 : BaseDay
+    public override string PartOne(string input)
     {
-        public override string PartOne(string input)
+        var diag = input.Lines().ToList();
+
+        var gamma = Convert.ToInt64(GetGamma(diag), 2);
+        var epsilon = Convert.ToInt64(GetEpsilon(diag), 2);
+
+        return (gamma * epsilon).ToString();
+    }
+
+    private string GetGamma(List<string> diag)
+    {
+        var result = string.Empty;
+
+        for (var i = 0; i < diag.First().Length; i++)
         {
-            return string.Empty;
+            var oneCount = diag.Count(x => x[i] == '1');
+            var zeroCount = diag.Count(x => x[i] == '0');
+
+            if (oneCount >= zeroCount)
+            {
+                result += "1";
+            }
+            else
+            {
+                result += "0";
+            }
         }
 
-        public override string PartTwo(string input)
+        return result;
+    }
+
+    private string GetEpsilon(List<string> diag)
+    {
+        var result = string.Empty;
+
+        for (var i = 0; i < diag.First().Length; i++)
         {
-            return string.Empty;
+            var oneCount = diag.Count(x => x[i] == '1');
+            var zeroCount = diag.Count(x => x[i] == '0');
+
+            if (oneCount >= zeroCount)
+            {
+                result += "0";
+            }
+            else
+            {
+                result += "1";
+            }
         }
+
+        return result;
+    }
+
+
+    public override string PartTwo(string input)
+    {
+        var oxygen = input.Lines().ToList();
+        var scrubber = input.Lines().ToList();
+        var pos = 0;
+
+        while (oxygen.Count() > 1)
+        {
+            var gamma = GetGamma(oxygen)[pos];
+
+            oxygen = oxygen.Where(x => x[pos] == gamma).ToList();
+            pos++;
+        }
+
+        pos = 0;
+        while (scrubber.Count() > 1)
+        {
+            var epsilon = GetEpsilon(scrubber)[pos];
+
+            scrubber = scrubber.Where(x => x[pos] == epsilon).ToList();
+            pos++;
+        }
+
+        return (Convert.ToInt64(oxygen.First(), 2) * Convert.ToInt64(scrubber.First(), 2)).ToString();
     }
 }
