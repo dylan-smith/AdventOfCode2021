f4dc919b0a16bfc34dfec44e95edd51110f5cdbe
Dylan Smith <dylan-smith@github.com>
2021-12-03 23:31:08 -0600

    Day 4

--- src/Days/Day04.cs
+++ src/Days/Day04.cs
@@ -9,12 +9,132 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            return string.Empty;
+            var numbers = input.Lines().First().Integers().ToList();
+            var boards = ParseBoards(input.Lines().Skip(1).ToList());
+
+            foreach (var number in numbers)
+            {
+                foreach (var board in boards)
+                {
+                    MarkBoard(board, number);
+                }
+
+                if (boards.Any(IsWinner))
+                {
+                    var winner = boards.First(IsWinner);
+                    var score = ScoreBoard(winner, number);
+
+                    return score.ToString();
+                }
+            }
+
+            return "NOT FOUND!";
+        }
+
+        private void MarkBoard(int[,] board, int number)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    if (board[x, y] == number)
+                    {
+                        board[x, y] = -1;
+                    }
+                }
+            }
+        }
+
+        private List<int[,]> ParseBoards(List<string> lines)
+        {
+            var boards = new List<int[,]>();
+
+            while (lines.Any())
+            {
+                boards.Add(ParseBoard(lines.Take(5).ToList()));
+                lines = lines.Skip(5).ToList();
+            }
+
+            return boards;
+        }
+
+        private int[,] ParseBoard(List<string> lines)
+        {
+            var result = new int[5, 5];
+            var boardLines = lines.Take(5).ToList();
+
+            for (var y = 0; y < 5; y++)
+            {
+                var line = boardLines[y].Integers().ToList();
+
+                for (var x = 0; x < 5; x++)
+                {
+                    result[x, y] = line[x];
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsWinner(int[,] board)
+        {
+            for (var x = 0; x < 5; x++)
+            {
+                if (board[x, 0] == -1 && board[x, 1] == -1 && board[x, 2] == -1 && board[x, 3] == -1 && board[x, 4] == -1)
+                {
+                    return true;
+                }
+
+                if (board[0, x] == -1 && board[1, x] == -1 && board[2, x] == -1 && board[3, x] == -1 && board[4, x] == -1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private int ScoreBoard(int[,] board, int number)
+        {
+            var score = 0;
+
+            for (var x = 0; x < 5; x++)
+            {
+                for (var y = 0; y < 5; y++)
+                {
+                    if (board[x, y] > 0)
+                    {
+                        score += board[x, y];
+                    }
+                }
+            }
+
+            return score * number;
         }
 
         public override string PartTwo(string input)
         {
-            return string.Empty;
+            var numbers = input.Lines().First().Integers().ToList();
+            var boards = ParseBoards(input.Lines().Skip(1).ToList());
+
+            foreach (var number in numbers)
+            {
+                foreach (var board in boards)
+                {
+                    MarkBoard(board, number);
+                }
+
+                if (boards.Count == 1 && IsWinner(boards.First()))
+                {
+                    var score = ScoreBoard(boards.First(), number);
+
+                    return score.ToString();
+                }
+                
+                boards = boards.Where(x => !IsWinner(x)).ToList();
+            }
+
+            return "NOT FOUND!";
         }
     }
 }
