92e555815e9899f85e4cadba5810038df3eae911
Dylan Smith <dylan-smith@github.com>
2021-12-06 18:34:44 -0600

    namespace formatting

--- src/Days/Day06.cs
+++ src/Days/Day06.cs
@@ -1,40 +1,39 @@
-﻿namespace AdventOfCode.Days
+﻿namespace AdventOfCode.Days;
+
+[Day(2021, 6)]
+public class Day06 : BaseDay
 {
-    [Day(2021, 6)]
-    public class Day06 : BaseDay
+    public override string PartOne(string input)
     {
-        public override string PartOne(string input)
-        {
-            var fish = InitFish(input);
-            80.Times(() => fish = SimulateFish(fish));
+        var fish = InitFish(input);
+        80.Times(() => fish = SimulateFish(fish));
 
-            return fish.Sum(x => x.Value).ToString();
-        }
+        return fish.Sum(x => x.Value).ToString();
+    }
 
-        public override string PartTwo(string input)
-        {
-            var fish = InitFish(input);
-            256.Times(() => fish = SimulateFish(fish));
+    public override string PartTwo(string input)
+    {
+        var fish = InitFish(input);
+        256.Times(() => fish = SimulateFish(fish));
 
-            return fish.Sum(x => x.Value).ToString();
-        }
+        return fish.Sum(x => x.Value).ToString();
+    }
 
-        private IDictionary<int, long> InitFish(string input)
-        {
-            var inputFish = input.Integers().ToList();
-            IDictionary<int, long> fish = new Dictionary<int, long>();
-            inputFish.ForEach(f => fish.SafeIncrement(f));
+    private IDictionary<int, long> InitFish(string input)
+    {
+        var inputFish = input.Integers().ToList();
+        IDictionary<int, long> fish = new Dictionary<int, long>();
+        inputFish.ForEach(f => fish.SafeIncrement(f));
 
-            return fish;
-        }
+        return fish;
+    }
 
-        private IDictionary<int, long> SimulateFish(IDictionary<int, long> fish)
-        {
-            var newFish = new Dictionary<int, long>();
-            fish.ForEach(x => newFish.SafeSet((x.Key + 8) % 9, x.Value));
-            newFish.SafeIncrement(6, fish.SafeGet(0));
+    private IDictionary<int, long> SimulateFish(IDictionary<int, long> fish)
+    {
+        var newFish = new Dictionary<int, long>();
+        fish.ForEach(x => newFish.SafeSet((x.Key + 8) % 9, x.Value));
+        newFish.SafeIncrement(6, fish.SafeGet(0));
 
-            return newFish;
-        }
+        return newFish;
     }
 }
