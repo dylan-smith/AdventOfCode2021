f1a8cd558067bba4661f24f7b13172ff2d435e93
Dylan Smith <dylan-smith@github.com>
2021-12-08 23:28:59 -0600

    day 9

--- src/Days/Day09.cs
+++ src/Days/Day09.cs
@@ -1,20 +1,80 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Drawing;
 
-namespace AdventOfCode.Days
+namespace AdventOfCode.Days;
+
+[Day(2021, 9)]
+public class Day09 : BaseDay
 {
-    [Day(2021, 9)]
-    public class Day09 : BaseDay
+    public override string PartOne(string input)
     {
-        public override string PartOne(string input)
+        var heights = input.CreateCharGrid();
+        var result = 0;
+
+        for ( var x = 0; x <= heights.GetUpperBound(0); x++ )
         {
-            return string.Empty;
+            for (var y = 0; y <= heights.GetUpperBound(1); y++ )
+            {
+                if (IsLowPoint(x, y, heights))
+                {
+                    result += int.Parse(heights[x, y].ToString()) + 1;
+                }
+            }
         }
 
-        public override string PartTwo(string input)
+        return result.ToString();
+    }
+
+    private bool IsLowPoint(int x, int y, char[,] heights)
+    {
+        var neighbours = heights.GetNeighbors(x, y, false);
+
+        if (neighbours.All(n => int.Parse(n.ToString()) > int.Parse(heights[x, y].ToString())))
         {
-            return string.Empty;
+            return true;
+        }
+
+        return false;
+    }
+
+    public override string PartTwo(string input)
+    {
+        var heights = input.CreateCharGrid();
+        var lowPoints = GetLowPoints(heights);
+
+        var basins = lowPoints.Select(p => GetBasinSize(heights, p)).ToList();
+
+        var result = basins.OrderByDescending(b => b).Take(3).Multiply();
+
+        return result.ToString();
+    }
+
+    private int GetBasinSize(char[,] heights, Point p)
+    {
+        var basinPoints = new HashSet<Point>() { p };
+        var next = basinPoints.SelectMany(x => heights.GetNeighborPoints(x)).Where(x => heights[x.point.X, x.point.Y] != '9').Where(p => !basinPoints.Contains(p.point)).Select(p => p.point).ToList();
+        //var next = basinPoints.SelectMany(x => x.GetNeighbors(false)).Where(p => p.X >= 0 && p.Y >= 0).Where(x => heights[x.X, x.Y] != '9').Where(p => !basinPoints.Contains(p)).ToList();
+
+        while (next.Any())
+        {
+            basinPoints.AddRange(next);
+            next = basinPoints.SelectMany(x => heights.GetNeighborPoints(x)).Where(x => heights[x.point.X, x.point.Y] != '9').Where(p => !basinPoints.Contains(p.point)).Select(p => p.point).ToList();
+            //next = basinPoints.SelectMany(x => x.GetNeighbors(false)).Where(p => p.X >= 0 && p.Y >= 0).Where(x => heights[x.X, x.Y] != '9').Where(p => !basinPoints.Contains(p)).ToList();
+        }
+
+        return basinPoints.Count;
+    }
+
+    private IEnumerable<Point> GetLowPoints(char[,] heights)
+    {
+        for (var x = 0; x <= heights.GetUpperBound(0); x++)
+        {
+            for (var y = 0; y <= heights.GetUpperBound(1); y++)
+            {
+                if (IsLowPoint(x, y, heights))
+                {
+                    yield return new Point(x, y);
+                }
+            }
         }
     }
 }
