fe21e1373f7c9e907165c2904c9d7db172243f60
Dylan Smith <dylan-smith@github.com>
2021-12-10 11:05:21 -0600

    Day 10

--- src/Days/Day10.cs
+++ src/Days/Day10.cs
@@ -9,12 +9,96 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            return string.Empty;
+            var result = input.Lines().Sum(x => CalcSyntaxScore(x));
+
+            return result.ToString();
+        }
+
+        private long CalcSyntaxScore(string input)
+        {
+            var prev = input;
+            var next = input;
+
+            do
+            {
+                prev = next;
+                next = prev.Replace("()", string.Empty).Replace("[]", string.Empty).Replace("{}", string.Empty).Replace("<>", string.Empty);
+            } while (next.Length < prev.Length);
+
+            var corruptChar = next.FirstOrDefault(c => c is ')' or ']' or '}' or '>');
+
+            switch (corruptChar)
+            {
+                case default(char):
+                    return 0;
+                case ')':
+                    return 3;
+                case ']':
+                    return 57;
+                case '}':
+                    return 1197;
+                case '>':
+                    return 25137;
+                default:
+                    throw new Exception();
+            }
         }
 
         public override string PartTwo(string input)
         {
-            return string.Empty;
+            var lines = input.Lines().Where(x => CalcSyntaxScore(x) == 0).ToList();
+
+            var scores = lines.Select(x => CalcAutoCompleteScore(x)).OrderBy(x => x).ToList();
+
+            var result = scores[scores.Count / 2];
+
+            return result.ToString();
+        }
+
+        private long CalcAutoCompleteScore(string input)
+        {
+            var prev = input;
+            var next = input;
+
+            var closingChar = new Dictionary<char, char>();
+            closingChar.Add('(', ')');
+            closingChar.Add('[', ']');
+            closingChar.Add('{', '}');
+            closingChar.Add('<', '>');
+
+            do
+            {
+                prev = next;
+                next = prev.Replace("()", string.Empty).Replace("[]", string.Empty).Replace("{}", string.Empty).Replace("<>", string.Empty);
+            } while (next.Length < prev.Length);
+
+            var completion = string.Empty;
+
+            while (next.Length > 0)
+            {
+                completion += closingChar[next.Last()];
+                next = next.ShaveRight(1);
+            }
+
+            return GetCompletionScore(completion);
+        }
+
+        private long GetCompletionScore(string completion)
+        {
+            var score = 0L;
+            var charScores = new Dictionary<char, long>();
+            charScores.Add(')', 1);
+            charScores.Add(']', 2);
+            charScores.Add('}', 3);
+            charScores.Add('>', 4);
+
+            foreach (var c in completion)
+            {
+                score *= 5;
+                score += charScores[c];
+            }
+
+            return score;
         }
     }
 }
