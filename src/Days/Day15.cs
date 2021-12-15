73d0099adb3e12bed339813e09211b2f182975ac
Dylan Smith <dylan-smith@github.com>
2021-12-15 16:06:49 -0600

    15.1 perf, 3.5 seconds

--- src/Days/Day15.cs
+++ src/Days/Day15.cs
@@ -6,44 +6,80 @@ namespace AdventOfCode.Days;
 public class Day15 : BaseDay
 {
     private Dictionary<Point, long> _costs;
+    private int[,] _grid;
+    private long _maxCost = long.MaxValue;
 
     public override string PartOne(string input)
     {
-        var grid = input.CreateIntGrid();
+        _grid = input.CreateIntGrid();
 
-        var pos = new Point(grid.Width() - 1, grid.Height() - 1);
+        var pos = new Point(_grid.Width() - 1, _grid.Height() - 1);
         _costs = new Dictionary<Point, long>();
 
-        _costs.Add(pos, grid[pos.X, pos.Y]);
+        _maxCost = GetMaxCost();
 
-        CalcCosts(pos, grid);
+        CalcCosts(pos, _grid[pos.X, pos.Y]);
 
-        return (_costs[new Point(0, 0)] - grid[0, 0]).ToString();
+        return (_costs[new Point(0, 0)] - _grid[0, 0]).ToString();
     }
 
-    private void CalcCosts(Point pos, int[,] grid)
+    private long GetMaxCost()
     {
-        var neighbors = grid.GetNeighborPoints(pos);
-        var cost = _costs[pos];
-        var end = new Point(grid.Width() - 1, grid.Height() - 1);
+        var pos = new Point(0, 0);
+        var end = new Point(_grid.Width() - 1, _grid.Height() - 1);
+        var cost = 0L;
 
-        foreach (var n in neighbors.OrderBy(x => x.point.ManhattanDistance(end)))
+        while (pos != end)
         {
-            var ncost = cost + grid[n.point.X, n.point.Y];
+            var downCost = long.MaxValue;
+            var rightCost = long.MaxValue;
 
-            if (!_costs.ContainsKey(n.point))
+            if ((pos.Y + 1) < _grid.Height())
             {
-                _costs.Add(n.point, ncost);
-                CalcCosts(n.point, grid);
+                downCost = _grid[pos.X, pos.Y + 1];
+            }
+
+            if ((pos.X + 1) < _grid.Width())
+            {
+                rightCost = _grid[pos.X + 1, pos.Y];
+            }
+
+            if (downCost <= rightCost)
+            {
+                pos = new Point(pos.X, pos.Y + 1);
+                cost += downCost;
             }
             else
             {
-                if (_costs[n.point] > ncost)
-                {
-                    _costs[n.point] = ncost;
-                    CalcCosts(n.point, grid);
-                }
+                pos = new Point(pos.X + 1, pos.Y);
+                cost += rightCost;
             }
+        }
+
+        return cost;
+    }
+
+    private void CalcCosts(Point pos, long cost)
+    {
+        if (cost >= _maxCost) return;
+        if (_costs.ContainsKey(pos) && _costs[pos] <= cost) return;
+        _costs.SafeSet(pos, cost);
+
+        if (pos.X == 0 && pos.Y == 0)
+        {
+            if (cost < _maxCost)
+            {
+                _maxCost = cost;
+            }
+        }
+
+        var neighbors = _grid.GetNeighborPoints(pos).OrderBy(x => x.c).ToList();
+
+        foreach (var n in neighbors)
+        {
+            var ncost = cost + _grid[n.point.X, n.point.Y];
+
+            CalcCosts(n.point, ncost);
         }
     }
 
