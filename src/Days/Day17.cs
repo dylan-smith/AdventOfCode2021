6bae9fba6b89ae8b2da0f539be7dd3254e8e5628
Dylan Smith <dysmith@microsoft.com>
2022-12-01 10:54:14 -0500

    Day 17 part 1

--- src/Days/Day17.cs
+++ src/Days/Day17.cs
@@ -8,7 +8,45 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            return string.Empty;
+            var yDelta = 117;
+            var y = 0;
+            var steps = 0;
+            var yMax = 0;
+
+            while (y != -118)
+            {
+                y += yDelta;
+                yDelta--;
+                steps++;
+
+                if (y >= yMax)
+                {
+                    yMax = y;
+                }
+            }
+
+            base.Log($"yMax: {yMax}");
+            base.Log($"steps: {steps}");
+
+            for (var xDelta = 1; xDelta <= 259; xDelta++)
+            {
+                var delta = xDelta;
+                var x = 0;
+
+                while (x < 235)
+                {
+                    x += delta;
+                    delta--;
+                }
+
+                if (x is >= 235 and <= 259)
+                {
+                    base.Log($"xDelta: {xDelta}");
+                    break;
+                }
+            }
+
+            return yMax.ToString();
         }
 
         public override string PartTwo(string input)
